00ede9cb7144e99326326b2d2b25f17f5dcac2e4
Ray Zana <rayzana280@gmail.com>
2023-01-11 15:10:31 -0500

    commit tree 53240d971cd2cff2de33d2a88884f172619c3e28

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+const mainRemove = document.getElementsByTagName('main')
+mainRemove[0].remove();
+
+const newHeader = document.createElement('h1')
+newHeader.id = 'victory' 
+newHeader.textContent = 'ray is the champion'
+document.body.append(newHeader)
+
+
+console.log(newHeader)
+
